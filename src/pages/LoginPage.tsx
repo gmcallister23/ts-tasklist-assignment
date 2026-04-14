0f20c6d6f8df91c74673a30438cd2eba839598ea
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-04-14 10:44:10 -0400

    added welcome message and updated title on auth screen

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,8 @@ import LoginButton from '../components/LoginButton';
 const LoginPage = () => {
     return (
         <div>
-            <h1>Welcome</h1>
+            <h1>Welcome to Task Management</h1>
+            <p>Select either login or sign up to access your task list</p>
             <LoginButton />
         </div>
     );
