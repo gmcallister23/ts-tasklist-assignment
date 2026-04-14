2f37ba977e914158fbdcf44199b46998024c0bb4
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-04-13 21:22:02 -0400

    commented out navbar from page layout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,5 +1,5 @@
 import { Col, Container } from 'react-bootstrap';
-import NavBar from '../components/NavBar';
+//import NavBar from '../components/NavBar';
 
 type PageLayoutProps = {
     children?: React.ReactNode;
